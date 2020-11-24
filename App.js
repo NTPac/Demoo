5fbc76e6f42cc5b243f7d0e7fcf59578c4540636
NTPac <ntphong1999qnvn@gmail.com>
2020-11-24 09:36:25 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,15 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { StyleSheet, Text, View, Image, Linking } from 'react-native';
+import dmk from './FormDoiMK';
+
+
+ function Profile({ navigation }) {
+  return (
+      <View StyleSheet={styles.container}>
+          <Image StyleSheet={styles.image} source={require('./asset/girl1.jpg')}></Image>        
+          <Text StyleSheet={styles.Label}><b>Nguyen Van A</b></Text>
+          <Text>Ma sinh vien: 4051051110</Text>
+          <Text>ma khau: *******</Text>
+          <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('ChangePassword')}
+      />
+      </View>
+  )
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Profile">
+        <Stack.Screen name="Profile" component={Profile} />
+        <Stack.Screen name="ChangePassword" component={dmk} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -18,4 +43,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  image:{
+    width:200, 
+    height:200,
+    marginBottom:20,
+    borderTopLeftRadius:100,
+    borderTopRightRadius:100,
+    borderBottomLeftRadius:100,
+    borderBottomRightRadius:100,
+  }
+
+  Label: {
+    fontSize: 24,
+    
+  }
 });
